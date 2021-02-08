ceb962af1f9b1d573e01a7745e95e81bca0ae256
Shruti Sharma <shrutisharma.la15@gmail.com>
2021-02-08 12:18:09 -0800

    Sets WelcomeViewController for gifImageView and record video button tap event.

---
+++ GifMaker_Swift_Template/WelcomeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  WelcomeViewController.swift
+//  GifMaker_Swift_Template
+//
+//  Created by Shruti Sharma on 2/8/21.
+//  Copyright © 2021 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+  @IBOutlet private weak var gifImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    // An animated UIImage
+    let jeremyGif = UIImage.gif(name: "hotlineBling")
+    gifImageView.image = jeremyGif
+
+  }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
