798e102d46e9ad44cf941b3bc01220c52f770888
Shruti Sharma <shrutisharma.la15@gmail.com>
2021-02-08 11:28:40 -0800

    Initial commit

---
+++ GifMaker_Swift_Template/UIViewController+Record.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewController+Record.swift
+//  GifMaker_Swift_Template
+//
+//  Created by Shruti Sharma on 2/8/21.
+//  Copyright © 2021 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+extension UIViewController {
+  
+  @IBAction func launchVideoCamera(sender: AnyObject) {
+    
+  }
+}
