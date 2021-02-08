19669b4bdd083c9c492dd48c81390fe172dd1e8a
Shruti Sharma <shrutisharma.la15@gmail.com>
2021-02-08 12:16:56 -0800

    Creates an extension on UIViewController to launch image picker from record video button.

--- GifMaker_Swift_Template/UIViewController+Record.swift
+++ GifMaker_Swift_Template/UIViewController+Record.swift
@@ -9,9 +9,36 @@
 import UIKit
 import MobileCoreServices
 
-extension UIViewController {
-  
+extension  UIViewController {
+
   @IBAction func launchVideoCamera(sender: AnyObject) {
+    
+    let pickerController = UIImagePickerController()
+    pickerController.sourceType = .camera
+    pickerController.mediaTypes = [kUTTypeMovie as String]
+    pickerController.allowsEditing = false
+    pickerController.delegate = self
+    present(pickerController, animated: true)
+  }
+}
+
+extension UIViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  
+  public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+    dismiss(animated: true, completion: nil)
+  }
+  
+  public func imagePickerController(_ picker: UIImagePickerController,
+                                    didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    guard let mediaType = info[UIImagePickerController.InfoKey.mediaType] as? String,
+          mediaType == kUTTypeMovie as String else {
+      return
+    }
+    
+    if let videoURL = info[UIImagePickerController.InfoKey.mediaURL] as? URL {
+      UISaveVideoAtPathToSavedPhotosAlbum(videoURL.path, nil, nil, nil)
+      dismiss(animated: true)
+    }
     
   }
 }
