b124e3ca5473b13a6a9b7cf83b1a4307d2149d8f
Shruti Sharma <shrutisharma.la15@gmail.com>
2021-02-08 17:45:08 -0800

    Add functionality to resize view when keyboard appears and disappears from the screen.

--- GifMaker_Swift_Template/GifEditorViewController.swift
+++ GifMaker_Swift_Template/GifEditorViewController.swift
@@ -9,20 +9,104 @@
 import UIKit
 
 class GifEditorViewController: UIViewController {
-
-  @IBOutlet private weak var gifImageView: UIImageView!
-  var gifURL: URL?
   
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
+  @IBOutlet private weak var gifImageView: UIImageView!
+//  @IBOutlet private weak var captionTextField: UITextField! {
+//    didSet {
+//      captionTextField.delegate = self
+//    }
+//  }
+  
+  var gif: Gif?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
-    guard let gifURL = gifURL else {
-      return
-    }
-    let gifImage = UIImage.gif(url: gifURL.absoluteString)
-    gifImageView.image = gifImage
+    subscribeToKeyboardNotifications()
+    gifImageView.image = gif?.gifImage
   }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    unsubscribeFromKeyboardNotifications()
+  }
+  
+//  @IBAction func presentPreview(_ sender: UIButton) {
+//
+//    guard let gifPreviewVC = storyboard?.instantiateViewController(withIdentifier: "GifPreviewViewController")
+//            as? GifPreviewViewController else { return }
+//    guard let gif = gif, gif.videoURL != nil else { return }
+//    gif.caption = captionTextField.text
+//
+//    let regift = Regift(sourceFileURL: gif.videoURL!,
+//                        destinationFileURL: nil,
+//                        frameCount: frameCount,
+//                        delayTime: delaytTime,
+//                        loopCount: loopCount)
+//    let captionFont = captionTextField.font;
+//    guard let gifURL = regift.createGif(captionTextField.text, font: captionFont) else { return  }
+//
+//    let newGif = Gif(url: gifURL, videoURL: gif.videoURL!, caption: captionTextField.text!)
+//    gifPreviewVC.gif = newGif
+//    navigationController?.present(gifPreviewVC, animated: true, completion: nil)
+//
+//  }
+  
+  // MARK: - Observe and respond to keyboard notifications
+  
+  func subscribeToKeyboardNotifications() {
+    NotificationCenter.default.addObserver(self,
+                                           selector: #selector(keyboardWillShow),
+                                           name: UIResponder.keyboardWillShowNotification,
+                                           object: nil)
+    NotificationCenter.default.addObserver(self,
+                                           selector: #selector(keyboardWillHide),
+                                           name: UIResponder.keyboardDidHideNotification,
+                                           object: nil)
+  }
+  
+  func unsubscribeFromKeyboardNotifications() {
+    NotificationCenter.default.removeObserver(self,
+                                              name: UIResponder.keyboardWillShowNotification,
+                                              object: nil)
+    NotificationCenter.default.removeObserver(self,
+                                              name: UIResponder.keyboardDidHideNotification,
+                                              object: nil)
+  }
+  
+  @objc func keyboardWillShow(notification: NSNotification) {
+    if view.frame.origin.y >= 0 {
+      view.frame.origin.y -= getKeyboardHeight(notification: notification)
+    }
+  }
+  
+  @objc func keyboardWillHide(notification: NSNotification) {
+    if (view.frame.origin.y < 0) {
+      view.frame.origin.y += getKeyboardHeight(notification: notification)
+    }
+  }
+
+  func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+    let userInfo = notification.userInfo
+    let keyboardFrameEnd = userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue
+    let keyboardFrameEndRect = keyboardFrameEnd?.cgRectValue
+    return keyboardFrameEndRect?.size.height ?? 0
+  }
+
 }
+
+//// MARK: - UITextFieldDelegate
+//
+//extension GifEditorViewController: UITextFieldDelegate {
+//  func textFieldDidBeginEditing(_ textField: UITextField) {
+//    textField.placeholder = ""
+//  }
+//  
+//  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//    textField.resignFirstResponder()
+//    return true
+//  }
+//}
