98a5c4ccc7ed97d7a4b3c220e8f93e5bd78b2687
Shruti Sharma <shrutisharma.la15@gmail.com>
2021-02-08 21:44:45 -0800

    Adds share gif feature in GifPreviewViewController.

--- GifMaker_Swift_Template/GifPreviewViewController.swift
+++ GifMaker_Swift_Template/GifPreviewViewController.swift
@@ -17,5 +17,21 @@ class GifPreviewViewController: UIViewController {
     super.viewDidLoad()
     gifImageView.image = gif?.gifImage
   }
-
+  
+  @IBAction func shareGif(_ sender: UIButton) {
+    guard let gif = gif, let gifURL = gif.url else { return }
+    guard let animatedGif = try? Data(contentsOf: gifURL) else { return }
+    
+    let shareController = UIActivityViewController(activityItems: [animatedGif],
+                                                   applicationActivities: nil)
+    shareController.completionWithItemsHandler = { [weak self] activity, completed, items, error in
+      guard let self = self else { return }
+      if completed {
+        self.navigationController?.popToRootViewController(animated: true)
+        return
+      }
+    }
+    present(shareController, animated: true)
+  }
+  
 }
