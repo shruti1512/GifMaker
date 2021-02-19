74c46b92c042adc95a9a28522469200c116677d1
Shruti Sharma <shrutisharma.la15@gmail.com>
2021-02-19 15:57:25 -0800

    Adds GifDetailVC to show details of a gif selected on SavedGifsVC.

---
+++ GifMaker/GifDetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GifDetailViewController.swift
+//  GifMaker
+//
+//  Created by Shruti Sharma on 2/19/21.
+//  Copyright © 2021 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import UIKit
+
+class GifDetailViewController: UIViewController {
+  
+  @IBOutlet private weak var gifImageView: UIImageView!
+  public var gif: Gif?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    gifImageView.image = gif?.gifImage
+  }
+  
+  @IBAction private func closeView(_ sender: UIButton) {
+    dismiss(animated: true)
+  }
+  
+  @IBAction func shareGif(_ sender: UIButton) {
+    if let gifData = gif?.gifData { self.viewController(self, shareGif: gifData) }
+  }
+
+}
+
+extension GifDetailViewController: ShareGifProtocol { }
