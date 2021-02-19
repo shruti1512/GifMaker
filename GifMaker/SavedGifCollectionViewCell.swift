3b7b8a6f30e6587a755f761bcf451148367d3f7e
Shruti Sharma <shrutisharma.la15@gmail.com>
2021-02-18 21:18:29 -0800

    Adds a new view controller for SavedGifs screen and custom class for GifCollectionCell.

---
+++ GifMaker/SavedGifCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  SavedGifCollectionViewCell.swift
+//  GifMaker
+//
+//  Created by Shruti Sharma on 2/18/21.
+//  Copyright © 2021 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import UIKit
+
+class SavedGifCollectionViewCell: UICollectionViewCell {
+    
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
---
+++ GifMaker/SavedGifsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SavedGifsViewController.swift
+//  GifMaker
+//
+//  Created by Shruti Sharma on 2/18/21.
+//  Copyright © 2021 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import UIKit
+
+class SavedGifsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
